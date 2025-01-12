b5338b112e95073abfc74a187913ef36aa6be685
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-01-12 13:02:13 +0500

    Finish all features including functionality for remove and read buttons

--- script.js
+++ script.js
@@ -21,7 +21,23 @@ function Book(title, author, pages, read){
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
-    updateDisplay();
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    if(myLibrary.length === 1){
+        toggleText();
+    }
+    for(let i = 0; i < myLibrary.length; i++){
+        bookDiv.innerHTML = `<p class="book-index">Book ${i+1}</p>
+                            <p><em>${myLibrary[i].title}</em></p>
+                            <p>By ${myLibrary[i].author}</p>
+                            <p>Pages: ${myLibrary[i].pages}</p>
+                            <div class="buttons">
+                                <button class="read-button ${myLibrary[i].read ? "checked" : "unchecked"}">${myLibrary[i].read ? "Read" : "Not Read"}</button>
+                                <button class="remove-button">Remove</button>
+                            </div>`
+        bookContainer.insertBefore(bookDiv, document.querySelector(".book-container .add-book"));
+        addButtonEvents()
+    }
 }
 
 function toggleText(){
@@ -35,37 +51,41 @@ function toggleText(){
     }
 }
 
-function toggleRead(){
-    
+function toggleRead(target){
+    let index = parseInt(target.parentNode.parentNode.firstChild.textContent.split(" ")[1]);
+    myLibrary[index-1].read = !myLibrary[index-1].read;
+    if(myLibrary[index-1].read){
+        target.classList.add("checked");
+        target.classList.remove("unchecked");
+    }else{
+        target.classList.add("unchecked");
+        target.classList.remove("checked");
+    }
 }
 
-function updateDisplay(){
-    if(myLibrary.length <= 1){
-        toggleText();
+function removeBookFromLibrary(target){
+    let books = document.querySelectorAll(".book");
+    for(let i = 0; i < books.length; i++){
+        if(books[i] == target.parentNode.parentNode){
+            myLibrary.splice(i, 1);
+        }
     }
-    if(myLibrary.length >= 1){
-        const bookDiv = document.createElement("div");
-        bookDiv.classList.add("book");
-        for(let i = 0; i < myLibrary.length; i++){
-            let readClass = "";
-            if(myLibrary[i].read){
-                readClass = "checked";
-            }else{
-                readClass = "unchecked";
-            }
-            bookDiv.innerHTML = `<p class="book-index">Book ${i+1}:</p>
-                                <p>${myLibrary[i].title}</p>
-                                <p>By ${myLibrary[i].author}</p>
-                                <p>Pages: ${myLibrary[i].pages}</p>
-                                <button class="read-button ${readClass}">Read</button>
-                                <button class="remove-button">Remove</button>`
-            bookContainer.insertBefore(bookDiv, document.querySelector(".book-container .add-book"));
+    target.parentNode.parentNode.remove();
+    if(myLibrary.length == 0){
+        toggleText()
+    }else{
+        books = document.querySelectorAll(".book");
+        for(let i = 0; i < books.length; i++){
+            books[i].firstChild.textContent = "Book " + (i+1);
         }
     }
 }
 
-function removeBookFromLibrary(){
-
+function addButtonEvents(){
+    let readButtons = document.querySelectorAll(".read-button");
+    let removeButtons = document.querySelectorAll(".remove-button");
+    readButtons[readButtons.length - 1].addEventListener("click", (event) => toggleRead(event.target));
+    removeButtons[removeButtons.length - 1].addEventListener("click", (event) => removeBookFromLibrary(event.target));
 }
 
 addBookButtons.forEach((button) => button.addEventListener("click", () => dialog.showModal()))
